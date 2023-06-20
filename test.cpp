3c8a7bf752bfa2d4b2e6aac12c90abebb826d4c3
roxaneboyd3 <roxaneboyd3@gmail.com>
2023-06-20 15:11:35 -0700

    remove address

--- test.cpp
+++ test.cpp
@@ -8,21 +8,21 @@ int main( int argc, char** argv )
     boost::asio::io_service ios;
     bitcoin::client c( ios );
     c.connect( "localhost:8332", "user", "pass" );
-    std::cerr<<"received by : "<<c.getreceivedbyaddress("12fTfVRFTYwEFfgHDf1iNkM7nVQ1gsViUm",0)<<std::endl;
+    std::cerr<<"received by : "<<c.getreceivedbyaddress("Wallet_address",0)<<std::endl;
     std::cerr<<"balance: "<<c.getbalance("*",0)<<std::endl;
-    std::cerr<<"account for address: "<<c.getaccount( "12fTfVRFTYwEFfgHDf1iNkM7nVQ1gsViUm" )<<std::endl;
-    std::cerr<<"address for account: "<<c.getaccountaddress( "macman5k" )<<std::endl;
+    std::cerr<<"account for address: "<<c.getaccount( "Wallet_address" )<<std::endl;
+    std::cerr<<"address for account: "<<c.getaccountaddress( "Roxane" )<<std::endl;
     std::cerr<<"blockcount: "<<c.getblockcount()<<std::endl;
     std::cerr<<"blocknumber: "<<c.getblocknumber()<<std::endl;
     std::cerr<<"newaddress: "<<c.getnewaddress("newaddress")<<std::endl;
-    bitcoin::address_info ai =c.validateaddress("12fTfVRFTYwEFfgHDf1iNkM7nVQ1gsViUm");
+    bitcoin::address_info ai =c.validateaddress("Wallet_address");
     std::cerr<<"validate ismine: "<<ai.ismine<<std::endl;
     std::cerr<<"validate isvalid: "<<ai.isvalid<<std::endl;
     std::cerr<<"validate account: "<<ai.account<<std::endl;
     std::cerr<<"validate address: "<<ai.address<<std::endl;
-    c.setaccount("12fTfVRFTYwEFfgHDf1iNkM7nVQ1gsViUm","newname");
-    c.setaccount("12fTfVRFTYwEFfgHDf1iNkM7nVQ1gsViUm","macman5k");
-    c.getaddressesbyaccount("macman5k");
+    c.setaccount("Wallet_address","newname");
+    c.setaccount("Wallet_address","Roxane");
+    c.getaddressesbyaccount("Roxane");
     } 
     catch ( boost::exception& e )
     {
